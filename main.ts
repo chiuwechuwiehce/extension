da2a1c328d7900de22b08531bc3e94866dc1fa47
chiuwechuwiehce <93743144+chiuwechuwiehce@users.noreply.github.com>
2024-03-04 09:51:57 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
-namespace Special {
+//% color = "#AA278D" weight = 100
+namespace special {
+    /**
+     * Idk yet.
+     */
     export function thingy(): void{
         basic.showNumber(5)
     }

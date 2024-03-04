845e634d2bb74871c9426e9a850654484d1cde58
chiuwechuwiehce <93743144+chiuwechuwiehce@users.noreply.github.com>
2024-03-04 10:38:54 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,23 @@
 namespace extras {
     /**
-     * Idk yet.
+     * Lights a random tile.
      */
-    //% blockId="specialthingy"
+    //% blockId="specialrandomtile"
     //% block="light random tile"
     export function randomtile(): void{
         let randx = randint(0,4)
         let randy = randint(0,4)
         led.plot(randx,randy)
+    }
+    /**
+     * Lights a random tile. with random transparency.
+     */
+    //% blockId="specialrandomtiletransparency"
+    //% block="light random transparency tile"
+    export function randomtiletransparency(): void {
+        let randx = randint(0, 4)
+        let randy = randint(0, 4)
+        let randbright = randint(0,255)
+        led.plotBrightness(randx, randy,randbright)
     }
 }

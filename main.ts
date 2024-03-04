52dfe582f69933f12dfcea5b985631649286e93f
chiuwechuwiehce <93743144+chiuwechuwiehce@users.noreply.github.com>
2024-03-04 10:32:48 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,8 +5,8 @@ namespace extras {
     //% blockId="specialthingy"
     //% block="light random tile"
     export function randomtile(): void{
-        let randx = randint(1,5)
-        let randy = randint(1,5)
+        let randx = randint(0,4)
+        let randy = randint(0,4)
         led.plot(randx,randy)
     }
 }
